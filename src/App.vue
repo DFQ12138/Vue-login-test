fd2b3d208f9d06624a8b19b22926e63ee2c79013
DOU <doufengqi@outlook.com>
2021-11-21 20:52:26 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form v-if="!isReg">
+      <div>用户名:</div>
+      <input type="text" v-model="name">
+      <div>密码:</div>
+      <input type="password" v-model="password">
+      <button type="button" @click="login()">登录</button>
+      <button type="button" @click="reg()">注册</button>
+    </form>
+    <form v-else>
+      <div>用户名:</div>
+      <input type="text" v-model="name">
+      <div>密码:</div>
+      <input type="password" v-model="password">
+      <div>再次输入密码:</div>
+      <input type="password" v-model="repeat">
+      <button type="button" @click="addUser()">确定</button>
+      <button type="button" @click="cancel()">取消</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "login",
+  data() {
+    return {
+      isReg: false,
+      name: "",
+      password: "",
+      repeat: ""
+    };
+  },
+  methods: {
+    login() {
+      //验证姓名和密码是否与locastorage一致
+      if (
+          localStorage.getItem("name") === this.name &&
+          localStorage.getItem("password") === this.password
+      ) {
+        //清空输入框
+        this.name = "";
+        this.password = "";
+        // this.$router.push("/home/list");
+        this.push("/home/list");
+      } else {
+        alert('用户名或密码不正确');
+      }
+    },
+    reg() {
+      this.isReg = true;
+    },
+    cancel() {
+      this.isReg = false;
+    },
+    addUser() {
+      //验证两次输入密码是否一致
+      if (this.password === this.repeat) {
+        //将名字和密码存入localstorage中
+        localStorage.setItem("name", this.name);
+        localStorage.setItem("password", this.password);
+        //清空输入框
+        this.name = "";
+        this.password = "";
+        this.isReg = false;
+      } else {
+        alert("两次密码不一致");
+      }
+    }
   }
-}
+};
 </script>
+<!--
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped>
+</style>-->
